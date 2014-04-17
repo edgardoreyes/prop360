66f6edbe70bdec025d5ce4ecf14ac68f17e81347
edgardoreyes <edgar_v_reyes@yahoo.com>
2014-04-17 13:53:52 -0700

    ContacViewModels modified.

--- prop360.web/Models/ContactViewModels.cs
+++ prop360.web/Models/ContactViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
@@ -7,9 +8,18 @@ namespace prop360.web.Models
 {
     public class ContactUsViewModel
     {
+        [Required]
+        [Display(Name = "Name")]
         public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Url)]
         public string Website { get; set; }
+
         public string Comment { get; set; }
     }
 }
